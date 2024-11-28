28034bcb5da6838bb7f15d87ae8d97a80d49fded
david emioma <ue22aac@herts.ac.uk>
2024-11-28 18:13:26 +0000

    Fixed endpoint

--- app/api/recordings/[id]/processing/route.ts
+++ app/api/recordings/[id]/processing/route.ts
@@ -63,6 +63,7 @@ export async function POST(
             folderId: folder.id,
             source: fileName,
             userId: id,
+            processing: true,
           },
         },
       },
